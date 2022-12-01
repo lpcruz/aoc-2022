e5cf947cbf6264070edef977889a6528d36d632f
Laurence Cruz <lcruz@squarespace.com>
2022-12-01 08:58:51 -0500

    solution: day 1

--- src/day-1/solution.js
+++ src/day-1/solution.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
+
 const data = fs.readFileSync('input.txt', {encoding: 'utf-8'}).split('\n\n').map(elf => elf.split('\n').map(n => Number(n)));
 const calories = data.map(elf => elf.reduce((prev, cur) => prev + cur));
 const max = Math.max(...calories);
 const topThree = sumCalories.sort((a,b) => b-a).slice(0,3).reduce((prev,cur) => prev + cur);
-console.log({ max, topThree});
+console.log({ max, topThree });
