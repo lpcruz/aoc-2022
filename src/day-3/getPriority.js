a10057ce6d9b382f6ac9368ca51d651e2ab7472c
Laurence Cruz <lcruz@squarespace.com>
2022-12-03 13:13:53 -0500

    test: unit test for get priority...it.each is kinda slick but weird?

--- src/day-3/getPriority.js
+++ src/day-3/getPriority.js
@@ -1,15 +1,10 @@
-const alpha = Array.from(Array(26)).map((e, i) => i + 65);
-const upper = alpha.map((x) => String.fromCharCode(x));
+const upper = Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 65));
 const lower = upper.map(letter => letter.toLowerCase());
-const priorityMap = [...lower, ...upper].map((letter, idx) => ({ letter, priority: idx + 1 }));
-
-// super dirty, I don't like this but it's how my brain was working at the time.
-function getPriority(letter) {
-    const found = priorityMap.filter(pair => Object.keys(pair).find(key => pair[key] === letter))[0];
-    const { priority } = found;
-    return priority;
-}
+const letters = [...lower, ...upper];
+const getPriority = (letter) => letters.indexOf(letter) + 1;
 
 module.exports = {
-   getPriority
+   getPriority,
+   upper,
+   lower,
 }
--- src/day-3/solution-part-1.js
+++ src/day-3/solution-part-1.js
@@ -3,6 +3,12 @@ const fs = require('fs');
 const { getPriority } = require('./getPriority');
 const { sumItems } = require('./sumItems');
 
+function meets(a, b) {
+    a = Array.from(new Set(a));
+    b = Array.from(new Set(b));
+    return Array.from(a.filter(v => b.includes(v)));
+};
+
 const strings = fs.readFileSync('input.txt', {encoding: 'utf-8'}).trim().split('\n')
 const divide = list => {
     const half = Math.ceil(list.length / 2 );
@@ -12,14 +18,6 @@ const divide = list => {
     };
 };
 const data = strings.map(string => string.split('')).map(combination => divide(combination));
-
-// compare the first and second halves and map them to a single list
-const commonLetters = data.map(set => {
-    const { firstHalf, secondHalf } = set;
-    const common = [...new Set (firstHalf.filter(value => secondHalf.includes(value)))].pop();
-    return common;
-});
-
-// get priority and sum them all up
-const priorities = commonLetters.map(getPriority);
+const commonLetter = data.map(({ firstHalf, secondHalf }) => meets(firstHalf, secondHalf).pop());
+const priorities = commonLetter.map(getPriority);
 console.log(sumItems(priorities));
