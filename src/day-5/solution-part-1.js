6a75cda9b32d459c44d86deff2437f8f7462ffa4
Laurence Cruz <lcruz@squarespace.com>
2022-12-05 17:13:56 -0500

    test: add unit tests

--- src/day-5/solution-part-1.js
+++ src/day-5/solution-part-1.js
@@ -7,6 +7,9 @@ const {
 } = require('./util');
 
 const input = fs.readFileSync('./input.txt', { encoding: 'utf-8'}).split('\n');
+
+console.log(input)
+
 let crates = getCrates(input);
 const instructions = getInstructions(input);
 
--- src/day-5/solution-part-2.js
+++ src/day-5/solution-part-2.js
@@ -11,6 +11,6 @@ let crates = getCrates(input);
 const instructions = getInstructions(input);
 
 for (const instruction of instructions)
-    crates = move(crates,instruction, isMovedAtOnce = true)
+    crates = move(crates, instruction, isMovedAtOnce = true)
 
 console.log (crates.map(x => x[x.length-1]).reduce((a,b) => a+b));
