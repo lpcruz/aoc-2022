af4f4f3c8da1564117bd77ebabba545da331198d
Laurence Cruz <lcruz@squarespace.com>
2022-12-05 16:40:18 -0500

    solution: day 5

---
+++ src/day-5/solution-part-2.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const { 
+    getCrates,
+    getInstructions,
+    move, 
+} = require('./util');
+
+const input = fs.readFileSync('./input.txt', { encoding: 'utf-8'}).split('\n');
+let crates = getCrates(input);
+const instructions = getInstructions(input);
+
+for (const instruction of instructions)
+    crates = move(crates,instruction, isMovedAtOnce = true)
+
+console.log (crates.map(x => x[x.length-1]).reduce((a,b) => a+b));
