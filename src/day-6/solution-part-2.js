8f4bec2ead0e33f02c8f058742faff46157f8ceb
Laurence Cruz <lcruz@squarespace.com>
2022-12-07 08:23:08 -0500

    solution: day 6

--- src/day-6/solution-part-2.js
+++ src/day-6/solution-part-2.js
@@ -6,14 +6,15 @@ const bufferStream = fs.readFileSync('input.txt', { encoding: 'utf-8' })
   .replace(/\r/g, "")
   .trim();
 
+const windowLength = 14;
 let window = [];
 for (let i = 0; i < bufferStream.length; i++) {
     window.push(bufferStream[i]);
-    if (window.length > 4) {
+    if (window.length > windowLength) {
         window.shift();
     }
-    if (window.length === 4 && isUnique(window)) {
-        console.log(i + 1);
-        break;
+    if (window.length === windowLength && isUnique(window)) {
+      console.log(i + 1);
+      break;
     }
 }
